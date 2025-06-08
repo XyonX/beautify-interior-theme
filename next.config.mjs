e89d31397cc3a6762f654766cee28230f8a60bf6
joydip chakraborty <33289572+XyonX@users.noreply.github.com>
2025-06-08 14:57:11 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // allow “dangerous” IP‑based host headers (for testing on an IP:port)
+  server: {
+    allowDangerousIpBasedRequests: true,
+  },
+};
 
 export default nextConfig;
